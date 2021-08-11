2b8cbc79dd53e6484099147ecdcb5a652e43a5c6
KaniaruIvy <69512969+KaniaruIvy@users.noreply.github.com>
2021-08-11 09:10:32 +0300

    Update Registration.tsx

--- components/Registration/Registration.tsx
+++ components/Registration/Registration.tsx
@@ -63,86 +63,71 @@ export default function Registration() {
     navigation.navigate("Login");
   };
 
-  return (
-    <View style={styles.container}>
-      <Image
-        source={require("../assets/logo.jpeg")}
-        style={styles.logo}
-        resizeMode="contain"
-      ></Image>
-      <Text style={{ alignSelf: "center", marginTop: 10, fontSize: 25 }}>
-        Register
-      </Text>
+  return(
+      
+<View style={styles.container}>
+<Image
+            source={require('../assets/logo.jpeg')}
+            style={styles.logo}
+            resizeMode="contain"
+          >
+          </Image>
+          <Text style={{ alignSelf:'center', marginTop: 10, fontSize: 25}}>Register</Text>
+
+       
+
+<View style={styles.action}>
+       <Ionicons name="person" color={'black'} size={15} />
+        <TextInput {...register('FirstName',{required:true})} placeholder="First Name" style={styles.textInput} autoCompleteType="name"/>
+        {errors.FirstName ? <Text>{errors.FirstName.message}</Text>:null}
+</View> 
+
+     
+
+<View style={styles.action}>
+        <Ionicons name="person" color={'black'} size={15} />
+        <TextInput {...register('LastName',{required:true})} placeholder="Last Name" style={styles.textInput} autoCompleteType="name"/>
+        {errors.LastName ? <Text>{errors.LastName.message}</Text>:null}
+</View>    
+     
 
       <View style={styles.action}>
-        <Ionicons name="person" color={"black"} size={15} />
-        <TextInput
-          {...register("FirstName", { required: true })}
-          placeholder="First Name"
-          style={styles.textInput}
-        />
-        {errors.FirstName ? <Text>{errors.FirstName.message}</Text> : null}
-      </View>
+      <MaterialIcons name="email" color={'black'} size={15} />
+        <TextInput {...register('Email',{required:true})} placeholder="Email" style={styles.textInput} autoCompleteType="email"
+         />
+        {errors.Email ? <Text>{errors.Email.message}</Text>:null}
+     </View>
 
-      <View style={styles.action}>
-        <Ionicons name="person" color={"black"} size={15} />
-        <TextInput
-          {...register("LastName", { required: true })}
-          placeholder="Last Name"
-          style={styles.textInput}
-        />
-        {errors.LastName ? <Text>{errors.LastName.message}</Text> : null}
-      </View>
+     
+   
+    <View style={styles.action}>
+    <Fontisto name="user-secret" color={'black'} size={15} />
+        <TextInput {...register('Password',{required:true, minLength:{value: 8, message:"must be 8 char"},
+         validate:(value: string)=>
+         {return[/[A-Z]/,/[a-z]/,/[0-9]/,/[^a-zA-z0-9]/,].every((pattern)=>pattern.test(value))|| "must include lower, upper, number and special characters";},
+         })} placeholder="Set Password" style={styles.textInput} autoCompleteType="password"
+         secureTextEntry={true} />
+        {errors.Password ? <Text>{errors.Password.message}</Text>:null}
+        </View>
 
-      <View style={styles.action}>
-        <MaterialIcons name="email" color={"black"} size={15} />
-        <TextInput
-          {...register("Email", { required: true })}
-          placeholder="Email"
-          style={styles.textInput}
-        />
-        {errors.Email ? <Text>{errors.Email.message}</Text> : null}
-      </View>
-
-      <View style={styles.action}>
-        <Fontisto name="user-secret" color={"black"} size={15} />
-        <TextInput
-          {...register("Password", {
-            required: true,
-            minLength: { value: 8, message: "must be 8 char" },
-            validate: (value: string) => {
-              return (
-                [/[A-Z]/, /[a-z]/, /[0-9]/, /[^a-zA-z0-9]/].every((pattern) =>
-                  pattern.test(value)
-                ) || "must include lower, upper, number and special characters"
-              );
-            },
-          })}
-          placeholder="Set Password"
-          style={styles.textInput}
-        />
-        {errors.Password ? <Text>{errors.Password.message}</Text> : null}
-      </View>
-
-      <View style={styles.action}>
-        <Fontisto name="user-secret" color={"black"} size={15} />
-        <TextInput
-          {...register("Cpassword", {
-            required: true,
-            validate: (value: {}) =>
-              value === password.current || "The passwords do not match",
-          })}
-          placeholder="Confirm Password"
-          style={styles.textInput}
-        />
-        {errors.Cpassword ? <Text>{errors.Cpassword.message}</Text> : null}
-      </View>
-      <TouchableOpacity
-        style={styles.commandButton}
-        onPress={handleSubmit(onSubmit)}
-      >
-        <Text style={styles.panelButtonTitle}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  );
+      
+   
+   
+    <View style={styles.action}>
+    <Fontisto name="user-secret" color={'black'} size={15} />
+        <TextInput {...register('Cpassword',{required:true,
+          validate: (value: {})=>
+            value === password.current|| "The passwords do not match",
+            })}  placeholder="Confirm Password" style={styles.textInput} autoCompleteType="password"
+            secureTextEntry={true}/>
+        {errors.Cpassword ? <Text>{errors.Cpassword.message}</Text>:null}
+        </View>
+        <TouchableOpacity style={styles.commandButton} onPress={handleSubmit(onSubmit)}>
+          <Text style={styles.panelButtonTitle}>Submit</Text>
+        </TouchableOpacity>
+      
+        </View>
+     
+       
+    );
 }
